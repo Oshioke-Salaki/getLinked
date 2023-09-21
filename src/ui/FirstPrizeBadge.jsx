0da3838ae5fa3f37657181649d10253a91ee6282
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-21 15:21:26 -0600

    update

--- src/ui/FirstPrizeBadge.jsx
+++ src/ui/FirstPrizeBadge.jsx
@@ -1,17 +1,8 @@
-import medal from "../assets/gold_medal 1.png";
-
 function FirstPrizeBadge() {
   return (
-    <div className="font-mons relative h-fit rounded-lg border-[1px] border-solid border-[#903AFF] bg-[#903aff1e] px-[23px] pb-[21px] pt-[149px] text-center">
-      <img
-        src={medal}
-        alt=""
-        className="absolute left-[0px] top-[-100px] !w-[660px]"
-      />
-      <h2 className="text-[40px] font-bold leading-[87px] text-white">1st</h2>
-      <h6 className="text-2xl font-semibold leading-[52px] text-white">
-        Runner
-      </h6>
+    <div className="font-mons rounded-lg border-[1px] border-solid border-[#903AFF] bg-[#903aff1e] px-[23px] pb-[21px] pt-[230px] text-center">
+      <h2 className="text-[40px] font-bold text-white">1st</h2>
+      <h6 className="text-2xl font-semibold text-white">Runner</h6>
       <h1 className="text-[32px] font-bold leading-[69px] text-[#903AFF]">
         N400,000
       </h1>
--- src/ui/PrizesSection.jsx
+++ src/ui/PrizesSection.jsx
@@ -2,23 +2,29 @@ import prizeMain from "../assets/prizeMain.png";
 import FirstPrizeBadge from "./FirstPrizeBadge";
 import SecondPrizeBadge from "./SecondPrizeBadge";
 import ThirdPrizeBadge from "./ThirdPrizeBadge";
+import medals from "../assets/medals.png";
 
 function PrizesSection() {
   return (
-    <div className="pr-140 pl-105 flex justify-center pb-[149px] pt-[74px]">
-      <img src={prizeMain} alt="" className="mt-[253px] h-[482px] w-[548px]" />
+    <div className="flex justify-center gap-x-[37px] pb-[149px] pl-[106px] pr-[129px] pt-[141px]">
+      <img src={prizeMain} alt="" className="mt-[179px] h-[482px] w-[548px]" />
       <div>
-        <h1 className=" text-[32px] font-bold text-white">
-          Prizes and
-          <br /> <span className="text-primaryColor">Rewards</span>
-        </h1>
-        <p className="font-mons mb-[150px] mt-4 text-sm font-normal leading-7 text-white">
-          Highlight of the prizes or rewards for winners and for participants
-        </p>
-        <div className="flex gap-x-[30px]">
-          <SecondPrizeBadge />
-          <FirstPrizeBadge />
-          <ThirdPrizeBadge />
+        <div className="pl-[149px]">
+          <h1 className=" text-[32px] font-bold text-white">
+            Prizes and
+            <br /> <span className="text-primaryColor">Rewards</span>
+          </h1>
+          <p className="font-mons mb-[83px] mt-4 text-sm font-normal leading-7 text-white">
+            Highlight of the prizes or rewards for winners and for participants
+          </p>
+        </div>
+        <div className="relative w-fit">
+          <img src={medals} className="absolute ml-4 h-[296px] w-[660px]" />
+          <div className="flex gap-x-[30px] pt-[130px]">
+            <SecondPrizeBadge />
+            <FirstPrizeBadge />
+            <ThirdPrizeBadge />
+          </div>
         </div>
       </div>
     </div>
