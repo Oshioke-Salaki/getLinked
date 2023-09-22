a636bd22dbda997b35dd122368671767f80c96b3
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-22 07:24:22 -0600

    update

--- src/ui/FaqSection.jsx
+++ src/ui/FaqSection.jsx
@@ -6,7 +6,7 @@ import darkPurpleStar from "../assets/starDarkPurple.svg";
 
 function FaqSection() {
   return (
-    <div className="border-b-borderColor relative flex flex-col items-center gap-x-[25px] overflow-hidden border-b-[1px] pb-[49px] pl-0 pr-0 pt-[60px] text-center sm:flex-row  sm:items-start sm:pb-[63px] sm:pl-[215px] sm:pr-[101px] sm:pt-[70px] sm:text-left">
+    <div className="border-b-borderColor relative flex flex-col items-center gap-y-[66px] overflow-hidden border-b-[1px] pb-[49px] pl-0 pr-0 pt-[60px] text-center sm:flex-row sm:items-start  sm:gap-x-[25px] sm:pb-[63px] sm:pl-[215px] sm:pr-[101px] sm:pt-[70px] sm:text-left">
       <TwinkleStar className="left-[183px] top-[110px]" star={darkPurpleStar} />
       <TwinkleStar
         className="left-[1020px] top-[97px]"
@@ -40,7 +40,11 @@ function FaqSection() {
           </FaqItem>
         </div>
       </div>
-      <img src={faq} alt="" className="h-[741px] w-[741px]" />
+      <img
+        src={faq}
+        alt=""
+        className="h-[327px] w-[327px] sm:h-[741px] sm:w-[741px]"
+      />
     </div>
   );
 }
