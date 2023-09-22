5a9de48fcc320d5a107161b44d9fc27968fea871
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-22 08:46:07 -0600

    update

--- src/ui/FirstPrizeBadge.jsx
+++ src/ui/FirstPrizeBadge.jsx
@@ -1,9 +1,13 @@
 function FirstPrizeBadge() {
   return (
-    <div className="font-mons mt-[30px] h-fit max-h-[374px] overflow-hidden rounded-lg border-[1px] border-solid border-[#903AFF] bg-[#903aff1e] px-[23px] pb-[21px] pt-[111px] text-center">
-      <h2 className="mt-[30px] text-[40px] font-bold text-white">1st</h2>
-      <h6 className="mb-[15px] text-2xl font-semibold text-white">Runner</h6>
-      <h1 className="text-[32px] font-bold leading-[69px] text-[#903AFF]">
+    <div className="font-mons mt-[30px] h-fit max-h-[147px] overflow-hidden rounded-lg border-[1px] border-solid border-[#903AFF] bg-[#903aff1e] px-2 pb-4 pt-[82px] text-center sm:max-h-[374px] sm:px-[23px] sm:pb-[21px] sm:pt-[111px]">
+      <h2 className="mt-[30px] text-xs font-bold text-white sm:text-[40px]">
+        1st
+      </h2>
+      <h6 className="mb-[15px] text-xs font-semibold text-white sm:text-2xl">
+        Runner
+      </h6>
+      <h1 className="text-sm font-bold leading-[30px] text-[#903AFF] sm:text-[32px] sm:leading-[69px]">
         N400,000
       </h1>
     </div>
