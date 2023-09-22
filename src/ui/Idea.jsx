88d814b0512edd82e2ff3758be71206452fb8c1c
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-22 02:23:09 -0600

    update

--- src/ui/Idea.jsx
+++ src/ui/Idea.jsx
@@ -6,7 +6,7 @@ import TwinkleStar from "./TwinkleStar";
 
 function Idea() {
   return (
-    <div className="border-b-borderColor relative flex justify-between gap-x-[60px] border-b-[1px] pb-[78px] pl-[230px] pr-[148px] pt-[62px]">
+    <div className="border-b-borderColor relative flex flex-col items-center justify-normal gap-y-[58px] border-b-[1px] pb-[78px]  pl-[230px] pr-[148px] pt-[29px] sm:flex-row sm:items-start sm:justify-between sm:gap-x-[60px] sm:pt-[62px]">
       <TwinkleStar
         className="left-[117px] top-[223px]"
         star={lightPurpleStar}
