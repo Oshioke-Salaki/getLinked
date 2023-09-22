314150c4af32d2da7f2338eec1cd40e96863210b
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-22 12:00:14 -0600

    update

--- src/ui/SponsorsSection.jsx
+++ src/ui/SponsorsSection.jsx
@@ -9,7 +9,7 @@ import flareRight from "../assets/flare4.png";
 
 function SponsorsSection() {
   return (
-    <div className="border-b-borderColor relative flex flex-col items-center border-b-[1px] bg-[#ffffff2] px-[128px] pb-[128px] pt-[114px] text-center">
+    <div className="border-b-borderColor relative flex flex-col items-center border-b-[1px] bg-[#ffffff2] px-[28px] pb-[128px] pt-[41px] text-center sm:px-[128px] sm:pt-[114px]">
       <img
         src={flareLeft}
         alt=""
@@ -20,16 +20,16 @@ function SponsorsSection() {
         alt=""
         className="absolute right-[-90px] top-[360px] bg-blend-hard-light"
       />
-      <h1 className="mb-[22px] text-[32px] font-bold leading-[86px] text-white">
+      <h1 className="mb-[5px] text-xl font-bold leading-7 text-white sm:mb-[22px] sm:text-[32px] sm:leading-[86px]">
         Partners and Sponsors
       </h1>
-      <p className="font-mons mb-[65px] text-sm font-normal leading-[28px] text-white">
+      <p className="font-mons mb-[65px] text-xs font-normal leading-[21px] text-white sm:text-sm sm:leading-[28px]">
         Getlinked Hackathon 1.0 is honored to have the following major
-        <br />
+        <br className="hidden sm:inline-block" />
         companies as its partners and sponsors
       </p>
-      <div className="grid grid-cols-[1fr_1fr_1fr] gap-x-[28px] rounded-[5px] border-[1px] border-[#D434FE] bg-[#ffffff2] px-[178px] py-[144px]">
-        <div className="border-b-solid border-r-solid after:bg-bgMain relative flex items-center justify-center border-b-[4px] border-r-[4px] border-b-[#D434FE] border-r-[#D434FE] pb-[26px] after:absolute after:bottom-[-4px] after:right-[-4px] after:block after:h-[27px] after:w-[27px]">
+      <div className="grid grid-cols-[1fr_1fr_1fr] gap-x-[28px] rounded-[5px] border-[1px] border-[#D434FE] bg-[#ffffff2] px-[48px] py-[38px] sm:px-[178px] sm:py-[144px]">
+        <div className="border-b-solid border-r-solid after:bg-bgMain relative flex items-center justify-center border-b-[2px] border-r-[2px] border-b-[#D434FE] border-r-[#D434FE] pb-[26px] after:absolute after:bottom-[-2px] after:right-[-2px] after:block after:h-[7px] after:w-[7px] sm:border-b-[4px] sm:border-r-[4px] sm:after:bottom-[-4px] sm:after:right-[-4px] sm:after:h-[27px] sm:after:w-[27px]">
           <img src={sponsor1}></img>
         </div>
         <div className="border-b-solid border-r-solid after:bg-bgMain relative flex items-center justify-center border-b-[4px] border-b-[#D434FE] px-[22px]">
@@ -48,7 +48,6 @@ function SponsorsSection() {
           <img src={sponsor6}></img>
         </div>
       </div>
-      ;
     </div>
   );
 }
