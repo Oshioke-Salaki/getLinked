ea9d306a9de26ada26c91dc3cd192ccafa1ea9e6
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-22 07:34:46 -0600

    update

--- src/ui/FaqItem.jsx
+++ src/ui/FaqItem.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 function FaqItem({ children }) {
   return (
-    <div className="flex border-b-[1px] border-b-[#D434FE] pb-[8px] sm:pb-[13px]">
+    <div className="flex border-b-[1px] border-b-[#D434FE] pb-[8px] text-left sm:pb-[13px] sm:text-left">
       <h1 className="font-mons flex w-full items-center justify-between text-xs font-normal leading-[18px] text-white sm:text-sm sm:leading-7">
         {children} <span className="text-xl text-[#D434FE]">+</span>
       </h1>
