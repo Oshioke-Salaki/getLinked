372a4c6086603fc0776553746c8a5465e0630a0e
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-22 01:57:30 -0600

    update

--- src/ui/PrimaryBtn.jsx
+++ src/ui/PrimaryBtn.jsx
@@ -2,7 +2,7 @@
 function PrimaryBtn({ children }) {
   return (
     <button
-      className=" inline-block w-fit rounded px-[52px] py-[17px] text-white"
+      className=" font-mons inline-block rounded px-[46px] py-[15px] text-[13px] font-normal text-white sm:px-[52px] sm:py-[17px] sm:text-base"
       style={{
         background:
           "linear-gradient(270deg, rgb(144, 58, 255) 0%, rgb(212, 52, 254) 56.42%, rgb(255, 38, 185) 99.99%, rgb(254, 52, 185) 100%)",
