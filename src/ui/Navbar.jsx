585bb387f45a9e9d665e0580b3806eeae157e959
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-22 01:17:46 -0600

    update

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 import PrimaryBtn from "./PrimaryBtn";
+import hamburger from "../assets/hamburgerIcon.svg";
 
 function Navbar() {
+  const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
+
+  function toggleMenu() {
+    setShowHamburgerMenu((show) => !show);
+  }
+
   return (
-    <nav className="border-b-borderColor flex items-center justify-between border-b-[1px] px-[128px] pb-[25px] pt-[63px]">
+    <nav className="border-b-borderColor flex items-center justify-between border-b-[1px] px-[48px] pb-[21px] pt-[36px] sm:px-[128px] sm:pb-[25px] sm:pt-[63px]">
       <Logo />
-      <div className="flex gap-[121px]">
+      <div className="hidden gap-[121px] sm:flex">
         <NavLinks />
         <PrimaryBtn>Register</PrimaryBtn>
       </div>
+      <button className="cursor-pointer" onClick={toggleMenu}>
+        <img
+          src={hamburger}
+          alt=""
+          className="flex h-[14px] w-[19px] sm:hidden"
+        />
+      </button>
+      {showHamburgerMenu && ""}
     </nav>
   );
 }
