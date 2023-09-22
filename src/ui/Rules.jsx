048effafd6a0fbbb4ed83e8bca31ede5401eec61
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-22 02:54:06 -0600

    update

--- src/ui/Rules.jsx
+++ src/ui/Rules.jsx
@@ -5,15 +5,15 @@ import TwinkleStar from "./TwinkleStar";
 
 function Rules() {
   return (
-    <div className="border-b-borderColor relative flex justify-between gap-x-[60px] overflow-hidden border-b-[1px] pl-[184px] pr-[50px]">
+    <div className="border-b-borderColor relative flex flex-col-reverse justify-start gap-x-[60px] gap-y-[10px] overflow-hidden border-b-[1px] pl-0 pr-0 sm:flex-row sm:justify-between sm:pl-[184px] sm:pr-[50px]">
       <TwinkleStar className="left-[439px] top-[116px]" star={greyStar} />
       <TwinkleStar className="left-[741px] top-[405px]" star={whiteStar} />
-      <div className="mt-[138px] max-w-[535px]">
-        <h1 className="text-[32px] font-bold text-white">
+      <div className="mt-0 sm:mt-[138px] sm:max-w-[535px]">
+        <h1 className="text-center text-xl font-bold leading-[27px] text-white sm:text-left sm:text-[32px] sm:leading-9">
           Rules and
           <br /> <span className="text-primaryColor">Guidelines</span>
         </h1>
-        <p className="font-mons mt-4 text-sm font-normal leading-7 text-white">
+        <p className="font-mons mt-[10px] px-[45px] text-center text-sm font-normal leading-7 text-white sm:mt-4 sm:px-0 sm:text-left">
           Our tech hackathon is a melting pot of visionaries, and its purpose is
           as clear as day: to shape the future. Whether you&apos;re a coding
           genius, a design maverick, or a concept wizard, you&apos;ll have the
@@ -22,7 +22,11 @@ function Rules() {
           that can change the world, that&apos;s what we&apos;re all about!
         </p>
       </div>
-      <img src={ruleImg} alt="" className="mt-[-20px] h-[664px] w-[664px]" />
+      <img
+        src={ruleImg}
+        alt=""
+        className="mt-[-20px] h-[382px] w-[294px] sm:h-[664px] sm:w-[664px]"
+      />
     </div>
   );
 }
