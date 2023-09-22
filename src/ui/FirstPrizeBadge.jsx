a149e54a9b04614be414aed788631aff530c2994
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-22 08:48:05 -0600

    update

--- src/ui/FirstPrizeBadge.jsx
+++ src/ui/FirstPrizeBadge.jsx
@@ -1,7 +1,7 @@
 function FirstPrizeBadge() {
   return (
     <div className="font-mons mt-[30px] h-fit max-h-[147px] overflow-hidden rounded-lg border-[1px] border-solid border-[#903AFF] bg-[#903aff1e] px-2 pb-4 pt-[82px] text-center sm:max-h-[374px] sm:px-[23px] sm:pb-[21px] sm:pt-[111px]">
-      <h2 className="mt-[30px] text-xs font-bold text-white sm:text-[40px]">
+      <h2 className="text-xs font-bold text-white sm:mt-[30px] sm:text-[40px]">
         1st
       </h2>
       <h6 className="mb-[15px] text-xs font-semibold text-white sm:text-2xl">
