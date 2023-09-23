e2ca91fc5055ff86de523d2a81fa9297aaa57538
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-23 00:02:45 -0600

    update

--- src/ui/SecondPrizeBadge.jsx
+++ src/ui/SecondPrizeBadge.jsx
@@ -1,8 +1,10 @@
 function SecondPrizeBadge() {
   return (
-    <div className="font-mons mt-[15px] h-fit max-h-[126px] overflow-hidden rounded-lg border-[1px] border-solid border-[#D434FE] bg-[#d434fe1e] px-[9px] pb-[23px] pt-[41px] text-center sm:mt-[80px] sm:max-h-[296px] sm:px-[25px] sm:pb-[33px] sm:pt-[90px]">
-      <h2 className="text-xs font-bold text-white sm:text-[36px] ">2nd</h2>
-      <h6 className="text-xs font-semibold text-white sm:mb-[13px] sm:text-2xl">
+    <div className="mt-[15px] h-fit max-h-[126px] overflow-hidden rounded-lg border-[1px] border-solid border-[#D434FE] bg-[#d434fe1e] px-[9px] pb-[23px] pt-[41px] text-center font-mons sm:mt-[80px] sm:max-h-[296px] sm:px-[22px] sm:pb-[45px] sm:pt-[75px]">
+      <h2 className="text-xs font-bold leading-[26px] text-white sm:text-[36px] sm:leading-[78px]">
+        2nd
+      </h2>
+      <h6 className="mt-[-20px] text-xs font-semibold leading-[26px] text-white sm:mb-[13px] sm:text-2xl sm:leading-[52px]">
         Runner
       </h6>
       <h1 className="text-sm font-bold text-[#903AFF] sm:text-[32px]">
