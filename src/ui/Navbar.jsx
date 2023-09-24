8dfdfe0705f642594d1197df0c2ff2f43dc3fe05
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-23 18:28:02 -0600

    update

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -56,16 +56,15 @@ function Navbar({ hasBorder }) {
             </button>
           </div>
         ) : (
-          <NavLink
-            to="/register"
+          <button
             className=" inline-block rounded px-[46px] py-[15px] font-mons text-[13px] font-normal text-white sm:px-[52px] sm:py-[17px] sm:text-base"
             style={{
               background:
                 "linear-gradient(270deg, rgb(144, 58, 255) 0%, rgb(212, 52, 254) 56.42%, rgb(255, 38, 185) 99.99%, rgb(254, 52, 185) 100%)",
             }}
           >
-            Register
-          </NavLink>
+            <NavLink to={"/register"}>Register</NavLink>
+          </button>
         )}
       </div>
       <button
