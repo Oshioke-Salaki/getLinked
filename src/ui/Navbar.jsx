7804e9c295cf7fcb444e2296bbb7971a44826436
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-23 18:29:32 -0600

    update

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -57,7 +57,7 @@ function Navbar({ hasBorder }) {
           </div>
         ) : (
           <button
-            className=" inline-block rounded px-[46px] py-[15px] font-mons text-[13px] font-normal text-white sm:px-[52px] sm:py-[17px] sm:text-base"
+            className=" z-30 inline-block rounded px-[46px] py-[15px] font-mons text-[13px] font-normal text-white sm:px-[52px] sm:py-[17px] sm:text-base"
             style={{
               background:
                 "linear-gradient(270deg, rgb(144, 58, 255) 0%, rgb(212, 52, 254) 56.42%, rgb(255, 38, 185) 99.99%, rgb(254, 52, 185) 100%)",
